cc232d13c0198f43ade68b407213a8117e244b70
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-29 11:01:35 +0530

    1

--- Find the Index of the First Occurrence in a String.cpp
+++ Find the Index of the First Occurrence in a String.cpp
@@ -1,32 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-int strStr(string h, string n) {
-        int ans=-1,temp;
-        bool flag=true;
-        for(int i=0;i<h.size();i++)
-        {
-            flag=true;
-            temp=i;
-            for(int j=0;j<n.size();j++)
-            {
-                if(h[temp++]!=n[j])
-                {
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
-             return i;
-
-        }
-        return ans;
+int strStr(string h, string n)
+{
+  int ans = -1, temp;
+  bool flag = true;
+  for (int i = 0; i < h.size(); i++)
+  {
+    flag = true;
+    temp = i;
+    for (int j = 0; j < n.size(); j++)
+    {
+      if (h[temp++] != n[j])
+      {
+        flag = false;
+        break;
+      }
     }
-
-
-
+    if (flag)
+      return i;
+  }
+  return ans;
+}
 
 int main()
 {
@@ -34,7 +29,7 @@ int main()
   cin >> tc;
   while (tc--)
   {
-    
+
     // Solution obj;
     // vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
     // for (auto i : ans)
@@ -46,4 +41,3 @@ int main()
   }
   return 0;
 }
-
